7fe05d54a485e2ef078e8e991476fdc600004e78
Mikulas <rullaf@gmail.com>
2014-08-18 21:58:18 +0200

    validate config

--- src/Clevis/PushNotifications/DI/PushNotificationsExtension.php
+++ src/Clevis/PushNotifications/DI/PushNotificationsExtension.php
@@ -4,6 +4,7 @@ namespace Clevis\PushNotifications\DI;
 
 use Clevis\PushNotifications\Device\Types;
 use Nette\DI\CompilerExtension;
+use Nette\Utils\Validators;
 
 
 class PushNotificationsExtension extends CompilerExtension
@@ -14,30 +15,53 @@ class PushNotificationsExtension extends CompilerExtension
 		$builder = $this->getContainerBuilder();
 
 		$config = $this->getConfig([
-			'android.gcm.useMultiCurl' => TRUE,
+			'android' => [
+				'c2dm' => [
+					'username' => NULL,
+					'password' => NULL,
+					'source' => NULL,
+				],
+				'gcm' => [
+					'apiKey',
+					'useMultiCurl' => TRUE,
+				],
+			],
+			'apple' => [
+				'sandbox' => NULL,
+				'pem' => NULL,
+				'passphrase' => NULL,
+			],
 		]);
+		Validators::assert($config['android']['c2dm']['username'], 'string', 'android.c2dm.username');
+		Validators::assert($config['android']['c2dm']['password'], 'string', 'android.c2dm.password');
+		Validators::assert($config['android']['c2dm']['source'], 'string', 'android.c2dm.source');
+		Validators::assert($config['android']['gcm']['apiKey'], 'string', 'android.gcm.apiKey');
+		Validators::assert($config['android']['gcm']['useMultiCurl'], 'boolean', 'android.gcm.useMultiCurl');
+		Validators::assert($config['apple']['sandbox'], 'boolean', 'apple.sandbox');
+		Validators::assert($config['apple']['pem'], 'string', "Key 'pem' must be relative or absolute path", 'apple.pem');
+		Validators::assert($config['apple']['passphrase'], 'string', 'apple.passphrase');
 
 		$builder->addDefinition($this->prefix('android.c2dm'))
 			->setClass('Clevis\PushNotifications\Service\OS\AndroidNotification')
 			->setArguments(array(
-				$config['android.c2dm.username'],
-				$config['android.c2dm.password'],
-				$config['android.c2dm.source'],
+				$config['android']['c2dm']['username'],
+				$config['android']['c2dm']['password'],
+				$config['android']['c2dm']['source'],
 			));
 
 		$builder->addDefinition($this->prefix('android.gcm'))
 			->setClass('Clevis\PushNotifications\Service\OS\AndroidGCMNotification')
 			->setArguments(array(
-				$config['android.gcm.apiKey'],
-				$config['android.gcm.useMultiCurl'],
+				$config['android']['gcm']['apiKey'],
+				$config['android']['gcm']['useMultiCurl'],
 			));
 
 		$builder->addDefinition($this->prefix('apple'))
 			->setClass('Clevis\PushNotifications\Service\OS\AppleNotification')
 			->setArguments(array(
-				$config['android.apple.sandbox'],
-				$config['android.apple.pem'],
-				$config['android.apple.passphrase'],
+				$config['apple']['sandbox'],
+				$config['apple']['pem'],
+				$config['apple']['passphrase'],
 			));
 
 		$builder->addDefinition($this->prefix('microsoft'))
